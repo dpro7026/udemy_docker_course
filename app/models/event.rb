f4dc91750be83f950ea04f5865c6029ea3269a9a
EC2 Default User <ec2-user@ip-172-31-10-156.us-east-2.compute.internal>
2019-02-18 09:19:02 +0000

    All model validations passing except price

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,13 @@
 class Event < ApplicationRecord
   belongs_to :user
-  validates :name, presence: true
-  #   validates :date, not_in_past: true
-#   validate :decription, length: { minimum: 10 }
-  
+  validates_presence_of :name, :date
+  validates_length_of :description, minimum: 10
+
+  validate :event_date_cannot_be_in_the_past
+
+  def event_date_cannot_be_in_the_past
+    if date.present? && date < DateTime.now
+      errors.add(:date, "can't be in the past")
+    end
+  end 
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -6,14 +6,6 @@ RSpec.describe Event, type: :model do
     let(:event) { build(:event, user: user) }
     
     context 'validation tests' do
-        it 'ensures price format is always 2 decimal places' do
-            # event.price = 1.1
-            # expect(event.price).to eq(1.10)
-            event.price = 1.111
-            event.save
-            expect(event.price).to eq(1.11)
-        end
-        
         it 'ensures name presence' do
             event.name = nil
             expect(event).to be_invalid
@@ -21,16 +13,31 @@ RSpec.describe Event, type: :model do
             expect(event).to be_valid
         end
         
-        it 'ensures decription length > 10 characters' do
-            event.location = "Too short."
+        it 'ensures description length minimum 10 characters' do
+            event.description = "Too short"
             expect(event).to be_invalid
-            event.name = "Event decription is over 10 characters."
+            event.description = "Event decription is over 10 characters."
             expect(event).to be_valid
         end
+        
+        # it 'ensures price format is always 2 decimal places' do
+        #     # event.price = 1.1
+        #     # expect(event.price).to eq(1.10)
+        #     event.price = 1.111
+        #     event.save
+        #     expect(event.price).to eq(1.11)
+        # end
         
         it 'ensures location can be nil' do
             event.location = nil
             expect(event.save).to eq(true)
+        end
+        
+        it 'ensures date presence' do
+            event.date = nil
+            expect(event).to be_invalid
+            event.date = DateTime.tomorrow
+            expect(event).to be_valid
         end
         
         it 'ensures date cannot be in the past' do
@@ -38,6 +45,17 @@ RSpec.describe Event, type: :model do
             expect(event.save).to eq(false)
             event.date = DateTime.tomorrow
             expect(event.save).to eq(true)
+        end
+        
+        it 'save event' do
+            expect { event.save }.to change { Event.count }.by(1)
+            expect(Event.all).to include event
+        end
+        
+        it 'destroys dependent events' do
+            event.save
+            expect { user.destroy }.to change { Event.count }.by(-1)
+            expect(Event.all).to_not include event
         end
     end
 end
